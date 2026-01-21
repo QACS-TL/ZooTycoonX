a7ef09f7f8ff173cbd816a6f0dac6d79db92bf1c
QACS-TL <PBehague@qa.com>
2026-01-21 15:50:19 +0000

    UnitTestsAdded

---
+++ ZooTycoonXTests/AnimalTests.cs
@@ -0,0 +1,47 @@
+﻿using ZooTycoonXLibrary;
+
+namespace ZooTycoonXTests
+{
+    public class AnimalTests
+    {
+        [Fact]
+        public void TestAnimalNameDefaultPropertyValue()
+        {
+            //Arrange + Act
+            Animal a = new Animal();
+
+            //Assert
+            Assert.Equal("Anon", a.Name);
+        }
+
+        [Fact]
+        public void TestAnimalNamePropertySetValue()
+        {
+            //Arrange
+            Animal a = new Animal();
+
+            //Act
+            a.Name = "Bob";
+
+            //Assert
+            Assert.Equal("Bob", a.Name);
+        }
+
+        [Theory]
+        [InlineData("B", "BX")]
+        [InlineData("", "XX")]
+        public void TestAnimalNamePropertySetToIllegalValue(string name, string expectedName)
+        {
+            //Arrange
+            Animal a = new Animal();
+
+            //Act
+            a.Name = name;
+
+            //Assert
+            Assert.Equal(expectedName, a.Name);
+        }
+
+
+    }
+}
